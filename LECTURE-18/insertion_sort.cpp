92a034c98453abbae74f03d957d968b3d2a1b342
nisheeth77 <abhaytripathi9901@gmail.com>
2022-10-09 22:36:15 +0530

    insertion sort

---
+++ LECTURE-18/insertion_sort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+void  insertion_sort(int arr[], int n)
+{
+    for(int i=1; i<n; i++)
+    {
+        int temp= arr[i];
+        int j=i-1;
+        while(j>=0)
+        {
+            if(arr[j]>=temp )
+            {
+                arr[j+1]=arr[j];
+            }
+            else{
+                break;
+            }
+            j--;
+        }
+        arr[j+1]=temp;
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    insertion_sort(arr,n);
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" "; 
+    }
+}
